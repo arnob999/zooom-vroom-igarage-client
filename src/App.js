68802d29639d3a3b17088208d7ecfea0f5544d0b
arnob999 <arnob196@gmail.com>
2023-04-14 00:15:26 +0600

    navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
+import { RouterProvider } from 'react-router-dom';
 import './App.css';
+import router from './Routes/Routes/Routes';
 
 function App() {
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold text-primary">
-        Hello world!
-      </h1>
+    <div className="max-w-[1440px] mx-auto">
+      <RouterProvider router={router} />
     </div>
   );
 }
--- src/Routes/Routes/Routes.js
+++ src/Routes/Routes/Routes.js
@@ -1,4 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
+import Main from "../../Layout/Main";
+import Home from "../../Pages/Home/Home";
 
 
 const router = createBrowserRouter([
